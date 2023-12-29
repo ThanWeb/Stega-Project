17d28e41b5f266e250af0732125a729a852226f3
Hans <classsickpeople@gmail.com>
2023-12-30 01:41:20 +0800

    Finishi distortion feature

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 import pathlib
 import os
+import cv2
+import numpy as np
 
 from tkinter import filedialog
-from PIL import Image
+from PIL import Image, ImageFilter
 from datetime import datetime
 
 root = tk.Tk()
@@ -42,7 +44,7 @@ def write_image(file_path, folder_path):
   os.rename(filename, decrypted_file_path)
 
 def start_encode_mode():
-  heading.config(text = "Encode Text to Picture")
+  heading.config(text = "Encode Text to Image")
   browse_image_button.pack(side = tk.TOP)
   browse_image_button.config(text = "Browse Image")
   browse_image_entry.pack(side = tk.TOP)
@@ -54,9 +56,10 @@ def start_encode_mode():
 
   decode_button.forget()
   decoded_string.forget()
+  distortion_button.forget()
 
 def start_decode_mode():
-  heading.config(text = "Decode Text from Picture")
+  heading.config(text = "Decode Text from Image")
   browse_image_button.pack(side = tk.TOP)
   browse_image_button.config(text = "Browse Image")
   browse_image_entry.pack(side = tk.TOP)
@@ -67,6 +70,21 @@ def start_decode_mode():
   string_entry_label.forget()
   string_entry.forget()
   encode_button.forget()
+  distortion_button.forget()
+
+def start_distortion_mode():
+  heading.config(text = "Image Distortion")
+  browse_image_button.pack(side = tk.TOP)
+  browse_image_button.config(text = "Browse Image")
+  browse_image_entry.pack(side = tk.TOP)
+  browse_image_entry.delete(0, tk.END)
+  distortion_button.pack(side = tk.TOP)
+
+  string_entry_label.forget()
+  string_entry.forget()
+  encode_button.forget()
+  decode_button.forget()
+  decoded_string.forget()
 
 def encode():
   input_image = browse_image_entry.get()
@@ -133,9 +151,41 @@ def decode():
 
   decoded_string.config(text = "Decoded string: " + res)
 
+def distortion():
+  input_image = browse_image_entry.get()
+
+  if input_image == "":
+    heading.config(text = "Please select an image", fg = "red")
+    return
+  
+  if is_image(input_image) == False:
+    heading.config(text = "This file is not an image", fg = "red")
+    return
+  
+  image = cv2.imread(input_image)
+  height, width = image.shape[:2]
+  distortion_map = np.zeros_like(image, dtype = np.float32)
+
+  for y in range(height):
+    for x in range(width):
+      new_x = (x + 10 * np.sin(2 * np.pi * y / 128.0)).astype(int)
+      new_y = (y + 10 * np.cos(2 * np.pi * x / 128.0)).astype(int)
+
+      if new_x >= 0 and new_x < width and new_y >= 0 and new_y < height:
+        distortion_map[y, x] = image[new_y, new_x]
+
+  distorted_img = cv2.remap(image, distortion_map[..., 0], distortion_map[..., 1], cv2.INTER_LINEAR)        
+  now = datetime.now()
+  ext = pathlib.Path(input_image).suffix
+  filename = now.strftime("%m-%d-%Y-%H-%M-%S" + ext)
+
+  cv2.imwrite("output/" + "distortion/" + filename, distorted_img)
+  heading.config(text = "Distoriton Success", fg = "green")
+
 menu_bar = tk.Menu(root)
-menu_bar.add_cascade(label = "Encode Text to Picture", command = start_encode_mode)
-menu_bar.add_cascade(label = "Decode Text from Picture", command = start_decode_mode)
+menu_bar.add_cascade(label = "Encode Text to Image", command = start_encode_mode)
+menu_bar.add_cascade(label = "Decode Text from Image", command = start_decode_mode)
+menu_bar.add_cascade(label = "Image Distortion", command = start_distortion_mode)
 menu_bar.add_cascade(label = "Exit", command = root.quit)
 
 browse_image_button = tk.Button(root, text = "Browse Image", command = browse_image, font = ('Arial', 8), padx = 8, pady = 4)
@@ -145,6 +195,7 @@ string_entry_label = tk.Label(root, text = "Enter string to encode")
 encode_button = tk.Button(root, text = "Encode", command = encode, font = ('Arial', 8), padx = 8, pady = 4, bg = "green", fg = "white")
 decode_button = tk.Button(root, text = "Decode", command = decode, font = ('Arial', 8), padx = 8, pady = 4, bg = "blue", fg = "white")
 decoded_string = tk.Label(root, text = "Decoded string : ...")
+distortion_button = tk.Button(root, text = "Distortion", command = distortion, font = ('Arial', 8), padx = 8, pady = 4, bg = "purple", fg = "white")
 
 root.config(menu = menu_bar)
 root.mainloop()
